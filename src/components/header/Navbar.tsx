8d2a290ff91eff5a9e2334b215e49f8c2e6eea9e
aboasil301 <aboasil207301@gmail.com>
2025-06-25 00:41:36 +0300

    feat: make Navbar fully responsive with mobile menu toggle and improved navigation UX

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import { Pages, Routes } from "@/constants/enums";
 import Link from "../link";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
+import { useState } from "react";
+import { Menu, XIcon } from "lucide-react"; // lucide-react is a popular icon library for React
 function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   const links = [
     {
       id: crypto.randomUUID(),
@@ -26,7 +32,27 @@ function Navbar() {
   ];
   return (
     <nav className="flex-1 justify-end flex">
-      <ul className="fixed lg:static top-0 px-10 py-20 lg:p-0 bg-background lg:bg-transparent transition-all duration-200 h-full lg:h-auto flex-col lg:flex-row w-full lg:w-auto flex items-start lg:items-center gap-10">
+      <Button
+        variant="secondary"
+        size="sm"
+        className="lg:hidden"
+        onClick={() => setOpenMenu(true)}
+      >
+        <Menu className="!w-6 !h-6" />
+      </Button>
+      <ul
+        className={`fixed lg:static ${
+          openMenu ? "left-0 z-50" : "-left-full"
+        } top-0 px-10 py-20 lg:p-0 bg-background lg:bg-transparent transition-all duration-200 h-full lg:h-auto flex-col lg:flex-row w-full lg:w-auto flex items-start lg:items-center gap-10`}
+      >
+        <Button
+          variant="secondary"
+          size="sm"
+          className="absolute top-10 right-10 lg:hidden"
+          onClick={() => setOpenMenu(false)}
+        >
+          <XIcon className="!w-6 !h-6" />
+        </Button>
         {links.map((link) => (
           <li key={link.id}>
             <Link
